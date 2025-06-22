fe2235d7ad0a8cc4bbf71aadfe8b266b8ac12a4f
AndersonBrasil <andersonbrasil72@gmail.com>
2025-06-22 19:46:38 -0300

    Push

--- public/build/assets/connectApi-DTGJlE87.js
+++
@@ -1,1 +0,0 @@
-import{a as o}from"./app-CgH7Q3V2.js";const a=o.create({baseURL:"http://localhost:8000/api/",headers:{"Access-Control-Allow-Origin":"*","Access-Control-Allow-Methods":"GET, POST, PATCH, PUT, DELETE, OPTIONS","Access-Control-Allow-Headers":"*","Content-Type":"application/json"}});export{a};
