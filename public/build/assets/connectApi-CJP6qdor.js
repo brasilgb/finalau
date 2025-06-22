1025409630ceb9cee3c53491803425a28496d2f7
AndersonBrasil <andersonbrasil72@gmail.com>
2025-06-22 19:24:15 -0300

    Push

---
+++ public/build/assets/connectApi-CJP6qdor.js
@@ -0,0 +1,1 @@
+import{a as o}from"./app-ioJI8ZeQ.js";const a=o.create({baseURL:"http://localhost:8000/api/",headers:{"Access-Control-Allow-Origin":"*","Access-Control-Allow-Methods":"GET, POST, PATCH, PUT, DELETE, OPTIONS","Access-Control-Allow-Headers":"*","Content-Type":"application/json"}});export{a};
---
+++ public/build/assets/welcome-DeSvEQ1n.js
@@ -0,0 +1,1 @@
+import{m as e,j as t}from"./app-ioJI8ZeQ.js";/* empty css            */function r(){return e.get("login"),t.jsx(t.Fragment,{})}export{r as default};
